784906c3773439d2825af4d99f4ab1c040085e7f
roy mustang <roy.mustang.net@gmail.com>
2018-04-20 11:52:25 +0800

    test

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(@NonNull Context context, ArrayList<Word> words) {
+    private int mResourceColorId;
+    public WordAdapter(@NonNull Context context, ArrayList<Word> words, int resourceColorId) {
         super(context, 0, words);
+        this.mResourceColorId = resourceColorId;
     }
 
     @NonNull
@@ -31,6 +34,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTextView.setText(currentWord.getMiwokTranslation());
         ImageView img = (ImageView) listItemView.findViewById(R.id.display_image);
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mResourceColorId);
+        textContainer.setBackgroundColor(color);
+
         // Check if an image is provided for this word or not
         if (currentWord.hasImage()) {
             // If an image is available, display the provided image based on the resource ID
